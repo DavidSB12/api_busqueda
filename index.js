e480da1ab1993d2584bfd78c911032eb0271100c
David Sanjaime <dsanjaime2002@gmail.com>
2024-01-04 21:51:56 +0100

    instalación cors

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 require('dotenv').config();
 const { connect, close } = require('./db/db.js');
 const app = express();
+const cors = require('cors');
 const port = 3004;
 const routes = require('./routes/routes.js');
+
+
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 
 // routes
 app.use('/', routes);
